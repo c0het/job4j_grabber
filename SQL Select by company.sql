4779744c0d5fe729f696b9092c229e366f08b6ef
c0het <c0het401@gmail.com>
2022-11-27 18:48:29 +0300

    2. SQL Select by company [#2760]

--- SQL Select by company.sql
+++ SQL Select by company.sql
@@ -17,32 +17,24 @@ INSERT INTO company (id, name) VALUES (1, 'ООО ПАН'),(2,'ОАО Звезд
 INSERT INTO person (id, name, company_id) VALUES (1, 'Иван', 4),(2,'Александр',4),(3,'Николай',5),
 	(4,'Никита',1),(5,'Роман',4),(6,'Федор',5),(7,'Дмитрий',3),(8,'Евгений',4),(9,'Петр',3),(10,'Светлана',5);
 	
-SELECT * FROM person WHERE company_id != 5;
-SELECT p.name,c.name FROM person as p, company as c WHERE p.company_id = c.id;
+SELECT person.name, company.name FROM person
+INNER JOIN company
+ON person.company_id = company.id AND company.id != 5
 
 
+SELECT company.name, COUNT(*) 
+FROM person, company
+WHERE person.company_id = company.id
+GROUP BY  person.company_id, company.name
+HAVING count(*) = 
+(
+	SELECT COUNT(person.name) 
+	FROM person 
+	GROUP BY person.company_id  
+	ORDER BY COUNT(person.name) DESC
+	LIMIT 1
+)
 
-SELECT company.name, rsl_count.count FROM company, 
-	(
-		SELECT company.id, company.name, MAX(rsl_count_for_max.count) FROM company,
-			(
-				SELECT company.id, company.name, person.company_id, COUNT(person.name) FROM person, company
-				WHERE person.company_id = company.id 
-				GROUP BY company.name, person.company_id, company.id
-			) 	
-		AS rsl_count_for_max
-		GROUP BY company.id, company.name
-	) 
-AS rsl_max,
-	
-	(
-		SELECT company.id, company.name, person.company_id, COUNT(person.name) FROM person, company
-		WHERE person.company_id = company.id 
-		GROUP BY company.name, person.company_id, company.id
-	) 
-AS rsl_count
-WHERE rsl_max.max = rsl_count.count AND company.id = rsl_count.id AND company.id = rsl_max.id
 
- 
 DROP TABLE person;
 DROP TABLE company;
